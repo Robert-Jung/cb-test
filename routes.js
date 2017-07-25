38704c3aa28b5f5a250ada425faaa0faa3003a82
Robert-Jung <rjinjung@gmail.com>
2017-07-24 17:49:43 -0700

    Adding person route.

--- routes.js
+++ routes.js
@@ -1,5 +1,32 @@
-var appRouter = function() {
+const PersonModel = require('./models').PersonModel
 
+var appRouter = function(app) {
+
+  app.get('/person', (req, res) => {
+    PersonModel.getAll(function(error, result) {
+      if(error) {
+        return res.status(400).send(error)
+      }
+      res.send(result)
+    })
+  })
+
+  app.get('/person/:personId', (req, res) => {
+
+  })
+
+  app.post('/person', (req, res) => {
+    PersonModel.save(req.body, function(error, result) {
+      if(error) {
+        return res.status(400).send(error)
+      }
+      res.send(result)
+    })
+  })
+
+  app.post('/comment', (req, res) => {
+
+  })
 }
 
 module.exports = appRouter
